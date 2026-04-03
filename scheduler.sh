88edc224e03a695888abf5bc38e404d40a85a6fe
PrintTrd <printtheerada@gmail.com>
2026-04-04 00:01:19 +0700

    chor: clean up comments and rename variable

--- scheduler.sh
+++ scheduler.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-set -e
-# 24 × 5s = 2 minutes max wait
-# RETRIES=0
+set -e # Exit immediately if a command exits with a non-zero status
 
 # Install Docker CLI tools if not already present
 if ! command -v docker >/dev/null 2>&1; then
@@ -9,18 +7,11 @@ if ! command -v docker >/dev/null 2>&1; then
     apk add --no-cache docker-cli docker-cli-compose
 fi
 
-echo "Waiting for database to be ready..."
-sleep "$DB_RETRY_DELAY"
-
-echo "Database is ready! Setting up cron jobs..."
-# Uses `docker run` against the already-built etl image so we do not duplicate orchestration logic inside the container.
-# `-p``: Project Name
+sleep "$DELAY"
 DIR=$(pwd)
+# Uses `docker run` against the already-built etl image to execute the ETL script every 2 minutes.
+# `-p``: Project Name
 echo "*/2 * * * * cd /app && docker compose -p brokerage-etl run --rm -v $DIR/data/input:/app/data/input:ro --no-deps etl" | crontab -
-# check log file exists and is writable
-# touch /var/log/etl-cron.log
-# docker compose exec scheduler ls -l /var/log/etl-cron.log
-
 echo "Cron jobs configured. Starting cron daemon..."
 # crond is Daemon (Background Process) that runs the scheduled tasks,
 # `-f`: keep it in foreground mode to prevent the container from exiting
